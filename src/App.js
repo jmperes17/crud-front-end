f79810219919155f2791f1985e189a65495c92ed
unknown <jmperes17@gmail.com>
2023-01-31 22:56:38 -0300

    criando tabela em react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+
+const API_URL = 'http://127.0.0.1:8000/api/users';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then(response => response.json())
+      .then(json => setData(json));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <table style={{ borderCollapse: 'collapse', width: '100%' }}>
+      <thead style={{ backgroundColor: '#f2f2f2' }}>
+        <tr>
+          <th style={{ border: '1px solid black', padding: '8px' }}>Nome</th>
+          <th style={{ border: '1px solid black', padding: '8px' }}>Telefone</th>
+          <th style={{ border: '1px solid black', padding: '8px' }}>E-mail</th>
+          <th style={{ border: '1px solid black', padding: '8px' }}>Data de Nascimento</th>
+          <th style={{ border: '1px solid black', padding: '8px' }}>Cidade de Nascimento</th>
+          <th style={{ border: '1px solid black', padding: '8px' }}>Empresas</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map(item => (
+          <tr key={item.id}>
+            <td style={{ border: '1px solid black', padding: '8px' }}>{item.name}</td>
+            <td style={{ border: '1px solid black', padding: '8px' }}>{item.telefone}</td>
+            <td style={{ border: '1px solid black', padding: '8px' }}>{item.email}</td>
+            <td style={{ border: '1px solid black', padding: '8px' }}>{item.dataDeNascimento}</td>
+            <td style={{ border: '1px solid black', padding: '8px' }}>{item.cidadeDeNascimento}</td>
+            <td style={{ border: '1px solid black', padding: '8px' }}>{item.empresas}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 }
 
